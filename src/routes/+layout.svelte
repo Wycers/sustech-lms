e033a1d962d4a27825dad48c1f250f32551ed737
wycers <wycers@gmail.com>
2024-03-08 14:40:27 +0800

    problem page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,20 @@
 <script>
+	import { ClassSwitcher, DashboardMainNav, Search, UserNav } from '$lib/components/dashboard';
 	import '../app.pcss';
 </script>
 
-<slot />
+<div class="hidden flex-col md:flex">
+	<div class="border-b">
+		<div class="flex h-16 items-center px-4">
+			<ClassSwitcher />
+			<DashboardMainNav class="mx-6" />
+			<div class="ml-auto flex items-center space-x-4">
+				<Search />
+				<UserNav />
+			</div>
+		</div>
+	</div>
+	<div class="flex-1 space-y-4 p-8 pt-6">
+		<slot />
+	</div>
+</div>
---
+++ src/routes/problem/[id=integer]/+page.server.ts
@@ -0,0 +1,21 @@
+import { db } from '$lib/db/db.server';
+import { problem } from '$lib/drizzle/schema';
+import { error, redirect, type ServerLoad } from '@sveltejs/kit';
+import { eq } from 'drizzle-orm';
+import _ from 'lodash';
+
+export const load: ServerLoad = async ({ params }) => {
+	const id = _.toNumber(params.id);
+	const p = await db.query.problem.findFirst({ where: eq(problem.id, id) });
+	console.log(p);
+
+	if (_.isNil(p)) {
+		error(404, {
+			message: 'Not found'
+		});
+	}
+
+	return {
+		data: p.description
+	};
+};
